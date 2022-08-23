2eb1e2b48491c7d10a2d5ea7ce9ae3f5f23a2286
Juan Pablo Vargas <vargasjp@outlook.com.ar>
2022-08-23 01:01:47 -0300

    Entrega Desafio

--- DesafioCoder/DesafioCoder/urls.py
+++ DesafioCoder/DesafioCoder/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from familiarpp.views import Familia
+from familiarpp.views import familia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('GrupoFamiliar/',Familia),
+    path('GrupoFamiliar/',familia),
 ]
--- DesafioCoder/familiarpp/views.py
+++ DesafioCoder/familiarpp/views.py
@@ -9,13 +9,13 @@ from django.http import HttpRequest
 # Create your views here.
 
 def familia(self):
-    madre=Familia(Nombre="Eleonora",Apellido="Bauza",Dni="17045100",FechaDeNac="01/24/1967")
+    madre=Familia(Nombre="Eleonora",Apellido="Bauza",Dni="17045100",FechaDeNac="1967-01-24")
     madre.save()
-    hijo=Familia(Nombre="Juan Pablo", Apellido="Vargas",Dni="37687400",FechaDeNac="07/14/1993")
+    hijo=Familia(Nombre="Juan Pablo", Apellido="Vargas",Dni="37687400",FechaDeNac="1993-07-14")
     hijo.save()
-    mascota=Familia(Nombre="Alfreda",Apellido="Vargas",Dni="47657876",FechaDeNac="10/25/2017")
+    mascota=Familia(Nombre="Alfreda",Apellido="Vargas",Dni="47657876",FechaDeNac="2017-10-25")
     mascota.save()
-    mascota2=Familia(Nombre="Cristina",Apellido="Bauza",Dni="53457873",FechaDeNac="10/15/2018")
+    mascota2=Familia(Nombre="Cristina",Apellido="Bauza",Dni="53457873",FechaDeNac="2018-10-15")
     mascota2.save()
     diccionario={"madre":madre,"hijo":hijo,"mascota":mascota,"mascota2":mascota2}
     plantilla=loader.get_template("template1.html")
